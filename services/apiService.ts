1504d1088ad05d3da1cf6e09e17e2c00b149412e
mero-seo <meroseoofficial@gmail.com>
2025-06-23 01:47:51 +0545

    Update API base URL in apiService to point to production environment

--- services/apiService.ts
+++ services/apiService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://192.168.1.73:3001/api/v1";
+const API_BASE_URL = "https://survey-backend-api.onrender.com/api/v1";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
